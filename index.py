9add988edc10d7c2e807842e57cb8775c71db902
Francisco2310 <74802078+Francisco2310@users.noreply.github.com>
2024-09-25 06:37:53 -0300

    onetimescrapingcorrection

--- index.py
+++ index.py
@@ -10,35 +10,40 @@ header = {
   'Accept-Language': 'pt-BR'
 }
 url = "https://store.steampowered.com/search/?filter=topsellers&ignore_preferences=1?supportedlang=brazilian"
-response = requests.get(url, headers=header)
 
-soup = BeautifulSoup(response.content, 'html.parser')
-div_principal = soup.select("div#search_resultsRows > a")
-mais_vendidos_preco = soup.select("div#search_resultsRows > a div.discount_final_price")
-precos = []
-titulos = []
-titulos_texto = []
-for elem in div_principal:
-    preco = []
-    preco.append(elem.select('div.discount_final_price'))
-    if preco[0]:
-        titulos.append(elem.select_one("span.title"))
-for preco in mais_vendidos_preco:
-    precos.append(preco.text)
-for item in titulos:
-    titulos_texto.append(item.text)
-data = {
-    'titulos': titulos_texto,
-    'precos': precos
-}
+def scraping():
+    response = requests.get(url, headers=header)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    div_principal = soup.select("div#search_resultsRows > a")
+    mais_vendidos_preco = soup.select("div#search_resultsRows > a div.discount_final_price")
+    precos = []
+    titulos = []
+    titulos_texto = []
+    for elem in div_principal:
+        preco = []
+        preco.append(elem.select('div.discount_final_price'))
+        if preco[0]:
+            titulos.append(elem.select_one("span.title"))
+    for preco in mais_vendidos_preco:
+        precos.append(preco.text)
+    for item in titulos:
+        titulos_texto.append(item.text)
+    data = {
+        'titulos': titulos_texto,
+        'precos': precos
+    }
+    return data
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", precos=precos, titulos=titulos_texto)
+    data = scraping()
+    return render_template("index.html", precos=data['precos'], titulos=data['titulos'])
 
 @app.route('/excel')
 def excel():
+    data = scraping()
     path = 'temp/temp_arquivo.xlsx'
     df = pd.DataFrame(data)
     df.to_excel(path, index=False)
@@ -51,6 +56,7 @@ def excel():
 
 @app.route('/app/api')
 def api():
+    data = scraping()
     return jsonify(data)
 
 
